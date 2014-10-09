f3dc3bdf3e8a9c9cca2edebe84e9845dd5f2b5a1
pcbaldwin <pbaldwin@clearfoundation.com>
2014-10-09 18:43:38 -0400

    Prepping for snapshot build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'bandwidth_viewer';
-$app['version'] = '2.0.0';
+$app['version'] = '2.0.1';
 $app['release'] = '1';
 $app['vendor'] = 'Tim Burgess';
 $app['packager'] = 'Tim Burgess';
--- packaging/app-bandwidth-viewer.spec
+++ packaging/app-bandwidth-viewer.spec
@@ -1,7 +1,7 @@
 
 Name: app-bandwidth-viewer
 Epoch: 1
-Version: 2.0.0
+Version: 2.0.1
 Release: 1%{dist}
 Summary: Bandwidth Viewer
 License: GPLv3
