3c15615a3da9a3d2ba46146927c2af5faf71d1b6
pcbaldwin <pbaldwin@clearfoundation.com>
2015-01-15 17:46:49 -0500

    Updated subcategory

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'bandwidth_viewer';
-$app['version'] = '2.0.5';
+$app['version'] = '2.0.14';
 $app['release'] = '1';
 $app['vendor'] = 'Tim Burgess';
 $app['packager'] = 'Tim Burgess';
@@ -19,8 +19,7 @@ $app['description'] = lang('bandwidth_viewer_app_description');
 
 $app['name'] = lang('bandwidth_viewer_app_name');
 $app['category'] = lang('base_category_reports');
-$app['subcategory'] = lang('base_category_network');
-
+$app['subcategory'] = lang('base_subcategory_performance_and_resources');
 
 /////////////////////////////////////////////////////////////////////////////
 // Controllers
--- packaging/app-bandwidth-viewer.spec
+++ packaging/app-bandwidth-viewer.spec
@@ -1,7 +1,7 @@
 
 Name: app-bandwidth-viewer
 Epoch: 1
-Version: 2.0.5
+Version: 2.0.14
 Release: 1%{dist}
 Summary: Bandwidth Viewer
 License: GPLv3
